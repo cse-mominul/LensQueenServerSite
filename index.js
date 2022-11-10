ca6bfedc8a9680703606b438a577fadf6def18cc
cse-mominul <cse.mominul.islam>
2022-11-11 02:34:19 +0600

    Added Mongobd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 // Middlewire
 app.use(cors());
@@ -13,11 +13,53 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l8spglx.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+try{
+    const serviceCollection = client.db("lensqueen").collection('services');
+    const reviewCollection = client.db("lensqueen").collection('reviews');
+    app.get('/services',async (req,res)=>{
+
+        const query ={}
+        const cursor = serviceCollection.find(query)
+        const services = await cursor.toArray();
+        res.send(services);
+    });
+    app.get('/reviews',async (req,res)=>{
+
+        const query ={}
+        const cursor = reviewCollection.find(query)
+        const reviews = await cursor.toArray();
+        res.send(reviews);
+
+    });
+    app.get('/services/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query ={_id: ObjectId(id)}
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+    })
+
+    app.post('/services', async(req, res)=>{
+        const newservice = req.body;
+        const result = await serviceCollection.insertOne(newservice);
+        res.send(result);
+    })
+    app.post('/reviews', async(req,res)=>{
+        const newReview = req.body;
+        const result = await reviewCollection.insertOne(newReview);
+        res.send(result)
+    })
+
+}
+finally{
+
+}
+}
+
+run().catch(err=>{
+    console.log(err)
+})
 
 app.get('/', (req, res)=>{
     res.send("hello from mongo server");
